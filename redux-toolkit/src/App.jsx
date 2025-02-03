4a40ec261c14fc0e453fdd44c98c5513b19fec1c
aman <amansaluja017@gmail.com>
2025-02-03 22:59:19 +0530

    add Redux setup with Todo functionality and Tailwind CSS integration

--- redux-toolkit/src/App.jsx
+++ redux-toolkit/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Todo, AddTodo } from './components/index'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <AddTodo />
+      <Todo />
     </>
   )
 }
